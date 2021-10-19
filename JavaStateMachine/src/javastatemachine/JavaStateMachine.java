1f21d8fb171b16965299baf7b6d4643b12e955ba
PorthoGamesBR <ub.com43+PorthoGamesBR@users.noreply.github.com>
2021-10-19 14:35:56 -0300

    Hello World test

--- JavaStateMachine/src/javastatemachine/JavaStateMachine.java
+++ JavaStateMachine/src/javastatemachine/JavaStateMachine.java
@@ -5,17 +5,12 @@
  */
 package javastatemachine;
 
-/**
- *
- * @author User
- */
+//Made by: Portho
+
 public class JavaStateMachine {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+       System.out.println("Hello World");
     }
     
 }
