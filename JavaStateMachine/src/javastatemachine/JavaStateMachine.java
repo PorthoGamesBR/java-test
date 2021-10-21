09e2399da9675cf3a49944c9f94b628af4e1ff82
PorthoGamesBR <ub.com43+PorthoGamesBR@users.noreply.github.com>
2021-10-21 20:31:08 -0300

    Added player and created enemy

--- JavaStateMachine/src/javastatemachine/JavaStateMachine.java
+++ JavaStateMachine/src/javastatemachine/JavaStateMachine.java
@@ -9,8 +9,13 @@ package javastatemachine;
 
 public class JavaStateMachine {
 
+	public static Player mainPlayer;
+	
     public static void main(String[] args) {
        System.out.println("Hello World");
+       
+       mainPlayer = new Player();
+       Enemy enm1 = new Enemy(new Position(-1,0));
     }
     
 }
